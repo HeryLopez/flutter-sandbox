e491d7242545362c5cf36cc95ba86d1e3da0f080
Hery Lopez <heryjlr@gmail.com>
2023-03-23 22:26:29 +0100

    complete codelabs

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -48,12 +50,148 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
+  void removeFavorite(index) {
+    favoritesSet.remove(favoritesSet.elementAt(index));
+    notifyListeners();
+  }
+
   bool isCurrentInFavorites() {
     return favoritesSet.contains(current);
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favoritesSet.isEmpty) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("No favorites yet."),
+        ],
+      );
+    } else {
+      final itemCount = appState.favoritesSet.length;
+
+      return ListView.builder(
+        itemCount: itemCount + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            // return the header
+            return Column(
+              children: [
+                const SizedBox(height: 16),
+                Text("You have $itemCount favorites:"),
+                const SizedBox(height: 10),
+              ],
+            );
+          }
+          index -= 1;
+
+          var item = appState.favoritesSet.elementAt(index);
+          return FavoriteItem(
+              item: item,
+              onTab: () {
+                appState.removeFavorite(index);
+              });
+        },
+      );
+    }
+  }
+
+// BuildContext context, int index, Animation<double> animation
+}
+
+class FavoriteItem extends StatelessWidget {
+  const FavoriteItem({
+    super.key,
+    required this.item,
+    required this.onTab,
+  });
+
+  final WordPair item;
+  final GestureTapCallback onTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: const Icon(Icons.favorite),
+        onTap: onTab,
+        title: Text("${item.first.capitalize()} ${item.second.capitalize()}"));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  Widget getPage() {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const GeneratorPage();
+        break;
+      case 1:
+        page = const FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    return page;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  // print('selected: $value');
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: getPage(),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -65,40 +203,32 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('A random name:'),
-            const SizedBox(
-              height: 10,
-            ),
-            BigCard(pair: pair),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    icon: Icon(icon),
-                    onPressed: () {
-                      appState.toggleFavorite();
-                    },
-                    label: const Text("Favorite")
-                ),
-                const SizedBox(width: 10,),
-                ElevatedButton(
-                    onPressed: () {
-                      appState.getNext();
-                    },
-                    child: const Text("Next")
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
